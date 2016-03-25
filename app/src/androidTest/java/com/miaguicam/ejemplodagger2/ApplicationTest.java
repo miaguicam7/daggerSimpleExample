87a48e20548eed296e7767664eeeecb7fa4c303a
Miguel Ángel Aguilar Campos <Miguel ngel Aguilar Campos>
2016-03-25 13:55:08 +0100

    Initial Commit

---
+++ app/src/androidTest/java/com/miaguicam/ejemplodagger2/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.miaguicam.ejemplodagger2;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
